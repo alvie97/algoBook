192b8f004905f8f9812b675852c48b453d95b68a
alvie97 <alfre_viera@hotmail.com>
2017-11-17 21:49:46 -0400

    Update union-find.cpp

--- Grafos/union-find.cpp
+++ Grafos/union-find.cpp
@@ -1,18 +1,47 @@
+// first implementation
 int P[100001];
 
 void init(){
     for (int i=0;i<100001;++i)
-        P[i]=i; //todos los nodos son padres de si mismos lo que representa un grafo disconexo
+        P[i]=i;
 }
 
-int Find(int x){
+int find(int x){
     if (P[x]==x) return x;
 
     return P[x] = Find(P[x]);
 }
 
-void Union(int x, int y){
+void merge(int x, int y){
     x = Find(x);
     y = Find(y);
     P[x]=y;
 }
+
+//second implementation - rank - path compression
+
+class ufd{
+    private:
+        vi p, rank;
+    public:
+        ufd(int n){
+            rank.assign(n, 0);
+            p.assign(n, 0);
+            for(int i = 0; i < n; i++)
+                    p[i] = i;
+        }
+        int find(int i){ return (p[i] == i)? i: (p[i] = find(p[i])); }
+        bool same_set(int x, int y) { return find(x) == find(y); }
+        void merge(int x, int y){
+            if (!same_set(x, y)){
+                int u = find(x); int v = find(y);
+                if (rank[u] > rank[v])
+                    p[v] = u;
+                else {
+                    p[u] = v;
+                    if (rank[u] == rank[v])
+                        ++rank[v];
+                }
+            }
+        }
+};
