8d28a9b3bdf28de6d3172efcdcaa14e54d8fd669
alfredo viera <alfre_viera@hotmail.com>
2018-05-18 18:15:38 -0400

    added num of disj sets and set size of to union find

--- Estructuras/union-find.cpp
+++ Estructuras/union-find.cpp
@@ -22,25 +22,33 @@ void merge(int x, int y){
 
 class ufd{
     public:
-        vi p, rank;
+        vi p, rank, set_size;
+	int disj_sets;
         ufd(int n){
             rank.assign(n, 0);
             p.assign(n, 0);
+	    set_size.assign(n, 1);
             for(int i = 0; i < n; i++)
                     p[i] = i;
+	    disj_sets = n;
         }
         int find(int i){ return (p[i] == i)? i: (p[i] = find(p[i])); }
         bool same_set(int x, int y) { return find(x) == find(y); }
         void merge(int x, int y){
             if (!same_set(x, y)){
                 int u = find(x); int v = find(y);
-                if (rank[u] > rank[v])
+                if (rank[u] > rank[v]) {
                     p[v] = u;
-                else {
+		    set_size[u] += set_size[v];
+		} else {
                     p[u] = v;
+		    set_size[v] += set_size[u];
                     if (rank[u] == rank[v])
                         ++rank[v];
                 }
+		--disj_sets;
             }
         }
+	int num_disj_sets() { return disj_sets; }
+	int set_size_of(int i) { return set_size[find(i)]; }
 };
